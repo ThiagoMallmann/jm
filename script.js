f866b5b23f34d941b6715cf6f8c53b7aa2d704b9
Victor Sevegnani <victor.sevegnani1702@gmail.com>
2025-12-01 18:48:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+    lucide.createIcons();
     const msg = document.createElement("div");
     msg.textContent = "Página carregada com sucesso";
     msg.className =
-        "fixed bottom-4 right-4 bg-blue text-white px-4 py-2 rounded-xl shadow-lg text-sm opacity-0 transition-opacity duration-700";
+        "fixed bottom-4 right-4 bg-blue text-white px-4 py-2 rounded-xl shadow-lg text-sm opacity-0";
 
     document.body.appendChild(msg);
 
