6ab131f411c8aa6289e60e9cd5a3b2280e2e909b
Victor Sevegnani <victor.sevegnani1702@gmail.com>
2025-12-01 16:02:00 -0300

    Melhorado html e js

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const msg = document.createElement("div");
+    msg.textContent = "Página carregada com sucesso";
+    msg.className =
+        "fixed bottom-4 right-4 bg-blue text-white px-4 py-2 rounded-xl shadow-lg text-sm opacity-0 transition-opacity duration-700";
+
+    document.body.appendChild(msg);
+
+    setTimeout(() => {
+        msg.style.opacity = "1";
+    }, 300);
+
+    setTimeout(() => {
+        document.body.removeChild(msg);
+    }, 4000)
+});
